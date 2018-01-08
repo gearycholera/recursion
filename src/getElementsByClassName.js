ac6e6fb763e21edd59773421123e44a1b688ca66
giri kalra <girikalra@gmail.com>
2018-01-08 01:30:52 -0800

    now passes all tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var output = [];
+
+	var recursiveSearch = function(item) {
+		if (item.classList !== undefined && item.classList.contains(className)) {
+			output.push(item);
+		};
+		for (var i = 0; i < item.childNodes.length; i++) {
+			recursiveSearch(item.childNodes[i]);
+		}
+	}
+	
+	recursiveSearch(document.body);
+
+	return output;
 };
